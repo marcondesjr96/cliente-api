d2e650fc00703557f26d41d6b336da44881fbd9f
Marcondes Júnior <jrmoriaproducoes1996@gmail.com>
2023-05-12 00:52:52 -0300

    create metod criar

--- src/main/java/com/github/marcondesjr96/cliente/services/impl/ClienteServiceImpl.java
+++ src/main/java/com/github/marcondesjr96/cliente/services/impl/ClienteServiceImpl.java
@@ -1,12 +1,19 @@
 package com.github.marcondesjr96.cliente.services.impl;
 
 import com.github.marcondesjr96.cliente.entities.Cliente;
+import com.github.marcondesjr96.cliente.repositories.ClienteRepository;
 import com.github.marcondesjr96.cliente.services.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteRepository clienteRepository;
 
     @Override
     public Cliente criar(Cliente cliente) {
-        return null;
+        return clienteRepository.save(cliente);
     }
 }
