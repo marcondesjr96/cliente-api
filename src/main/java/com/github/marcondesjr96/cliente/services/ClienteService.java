37cd10ddc6f8fd861272e3b345a3daade06cefd2
Marcondes Júnior <jrmoriaproducoes1996@gmail.com>
2023-05-12 00:51:10 -0300

    create service Cliente

---
+++ src/main/java/com/github/marcondesjr96/cliente/services/ClienteService.java
@@ -0,0 +1,8 @@
+package com.github.marcondesjr96.cliente.services;
+
+import com.github.marcondesjr96.cliente.entities.Cliente;
+
+public interface ClienteService {
+
+    Cliente criar(Cliente cliente);
+}
---
+++ src/main/java/com/github/marcondesjr96/cliente/services/impl/ClienteServiceImpl.java
@@ -0,0 +1,12 @@
+package com.github.marcondesjr96.cliente.services.impl;
+
+import com.github.marcondesjr96.cliente.entities.Cliente;
+import com.github.marcondesjr96.cliente.services.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+
+    @Override
+    public Cliente criar(Cliente cliente) {
+        return null;
+    }
+}
