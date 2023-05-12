1b917b051e95f1017581b7424d6607b9c9732e78
Marcondes Júnior <jrmoriaproducoes1996@gmail.com>
2023-05-12 00:56:20 -0300

    create metod criar Controller

--- src/main/java/com/github/marcondesjr96/cliente/controllers/ClienteController.java
+++ src/main/java/com/github/marcondesjr96/cliente/controllers/ClienteController.java
@@ -1,6 +1,10 @@
 package com.github.marcondesjr96.cliente.controllers;
 
+import com.github.marcondesjr96.cliente.entities.Cliente;
+import com.github.marcondesjr96.cliente.services.ClienteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/clientes")
 public class ClienteController {
 
+    private final ClienteService clienteService;
+
+    @GetMapping
+    public Cliente criar(@RequestBody Cliente cliente){
+        return clienteService.criar(cliente);
+    }
 }
